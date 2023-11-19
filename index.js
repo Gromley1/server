80891896edb7ef8815da6ac5f53e4ce06a66f335
gromley111 <gromley111@gmail.com>
2023-11-19 17:29:43 -0600

    allow all origins

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 const fastify = require("fastify")({
     logger: true,
-  }).register(require("./routes/clients"));
-  
-  // fastify.register(require('fastify-cors'), {
-  //     origin: '*',
-  //     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  // });
-  
-  const start = async () => {
+})
+const cors = require('@fastify/cors');
+
+fastify
+    .register(require("./routes/clients"))
+    .register(cors, {
+        origin: '*',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    });
+
+fastify;
+
+const start = async () => {
     try {
-      await fastify.listen({ port: 4200 });
+        await fastify.listen({ port: 4200 });
     } catch (err) {
-      fastify.log.error(err);
-      process.exit(1);
+        fastify.log.error(err);
+        process.exit(1);
     }
-  };
-  
-  start();
-  
+};
+
+start();
